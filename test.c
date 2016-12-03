7567c0d1437f7749c41b401daacdc15029ff121d
HymanLiu1987 <879651072@qq.com>
2016-12-03 21:37:16 +0800

    Win32 SDK基础（3）—— 从零开始创建windows窗口

--- test.c
+++
@@ -1,9 +0,0 @@
-#include "windows.h"
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,  
-                     _In_opt_ HINSTANCE hPrevInstance,  
-                     _In_ LPWSTR    lpCmdLine,  
-                     _In_ int       nCmdShow)   
-{
-  MessageBox(NULL,"Hello Win32","sdk",MB_ABORTRETRYIGNORE|MB_ICONERROR);
-  return 0;
-}
